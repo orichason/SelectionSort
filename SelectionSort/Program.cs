c37ee2c0dfb4734d91433d07946e3bfb2716389b
orichason <orichason@gmail.com>
2022-07-14 14:00:53 -0700

    Test

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        public static void SelectionSort(List<int> NumberList)
+        {
+            bool isSorted = false;
+
+            int count = 0;
+            int temp = 0;
+
+            while (!isSorted)
+            {
+
+                for (int i = 0; i < NumberList.Count; i++)
+                {
+                    for (int y = count; y < NumberList.Count - count; y++)
+                    {
+                        if (NumberList[i] < NumberList[y])
+                        {
+                            temp = NumberList[y];
+                        }
+                    }
+                    NumberList[i] = temp;
+                    count++;
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> UnsortedList = new List<int> { 43, 17, 5, 87, 29 };
+
+            SelectionSort(UnsortedList);
+        }
+    }
+}
